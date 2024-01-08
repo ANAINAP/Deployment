42df97508e9fe339513758a766f162e7fda2844d
ANAINAP <anainarajeev3@gmail.com>
2024-01-08 13:12:05 +0530

    2

---
+++ index.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(4000,()=>{
+    console.log("server running on 4000")
+}); //the server object listens on port 8080
