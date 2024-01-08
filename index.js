389e60c7db5537bff73924588fbbf09579885cf9
ANAINAP <anainarajeev3@gmail.com>
2024-01-08 15:29:31 +0530

    1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello Anaina!,good mrng'); //write a response to the client
+  res.write('Hello AnainaP!,good mrng'); //write a response to the client
   res.end(); //end the response
 }).listen(8080,()=>{
     console.log("server running on 8080")
