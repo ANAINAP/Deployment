e0bfbc02d2a32e8103812ec523e1c2166ef5c02e
ANAINAP <anainarajeev3@gmail.com>
2024-01-08 14:12:43 +0530

    check

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
-}).listen(4000,()=>{
-    console.log("server running on 4000")
+}).listen(8080,()=>{
+    console.log("server running on 8080")
 }); //the server object listens on port 8080
